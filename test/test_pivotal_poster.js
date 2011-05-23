bd8fee0e124ecb7298734e0fd89cda1edc58cdd0
Matthias Luebken <matthias@luebken.com>
2011-05-23 09:48:10 +0200

    first basic mocked http test

--- test/test_pivotal_poster.js
+++ test/test_pivotal_poster.js
@@ -1,6 +1,20 @@
+var http_mock = function() {};
 var vows = require('vows'),
 	assert = require('assert'),
-	pivotal_poster = require('../pivotal_poster')();
+	EventEmitter = require('events').EventEmitter,
+	pivotal_poster = require('../pivotal_poster')(http_mock);
+
+//mocks
+var request = function() {};
+request.write = function() {};
+request.end = function() {};
+var res = new EventEmitter();
+res.setEncoding = function() {};
+var http_callback;
+http_mock.request = function(options, cb) {
+	http_callback = cb;
+	return request;
+};
 	
 vows.describe('Pivotal Poster').addBatch({	
 	'prepare message for pivotal': {
@@ -15,6 +29,20 @@ vows.describe('Pivotal Poster').addBatch({
 		'should result in some stupid xml': function (err, post_msg, story_id) {
 			assert.equal ('123', story_id);
 			assert.equal (post_msg, '<source_commit><message>Branch:my branch\nsuper geil</message><author>der held vom erdbeerfeld</author><commit_id>631794a3473f51b1807da9c8be705c3ff6e35820</commit_id></source_commit>')
+		}
+	}
+	,
+	'post to pivotal' : {
+		topic: function () {
+			var message = "<message>hello</message>";
+			var story_id = "123";
+			pivotal_poster.postToPivotal(message, story_id, this.callback);
+			res.statusCode = 200;
+			http_callback(res);
+			res.emit('end');
+		},
+		'should do something' : function (msg) {
+			assert.equal ('Post success for 123', msg);
 		}
 	}
 })
